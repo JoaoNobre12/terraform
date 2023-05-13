02742f548b3cbd86dfb0530283378a1d7e51cbcf
= <=>
2023-05-13 01:52:16 -0300

    congigures remote state with backend

--- about-terraform-state/main.tf
+++ about-terraform-state/main.tf
@@ -1,9 +1,16 @@
 terraform {
   required_providers {
     aws = {
-        source  = "hashicorp/aws"
-        version = "~> 4.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
+  }
+
+  backend "s3" {
+    # if this field is blank, the cli asks for the values
+    bucket = "jn-state-200507340845"
+    key    = "dev/tfstate/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
@@ -12,21 +19,24 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "current" {
-  
+
 }
 
 resource "aws_s3_bucket" "jn-state-35" {
   bucket = "jn-state-${data.aws_caller_identity.current.account_id}"
-  versioning {
-    enabled = false
-  }
 
   tags = {
     Description = "stores tf state"
-    ManagedBy = "Terraform"
+    ManagedBy   = "Terraform"
   }
 }
 
+resource "aws_s3_bucket_versioning" "myversioning" {
+  bucket = aws_s3_bucket.jn-state-35.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
 output "remote-state-bucket-name" {
   value = aws_s3_bucket.jn-state-35.bucket
--- vpc-ec3/main.tf
+++ vpc-ec3/main.tf
@@ -1,9 +1,12 @@
 terraform {
   required_providers {
     aws = {
-        source  = "hashicorp/aws"
-        version = "~> 4.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
+  }
+  backend "s3" {
+    # terraform init -backend=true -backend-config=backend.hcl
   }
 }
 
@@ -15,12 +18,12 @@ resource "aws_vpc" "vpc_ec2" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
-   name = "my-terraform-vpc"
+    name = "my-terraform-vpc"
   }
 }
 
 resource "aws_subnet" "my-terraform-subnet-1" {
-  vpc_id = aws_vpc.vpc_ec2.id
+  vpc_id     = aws_vpc.vpc_ec2.id
   cidr_block = "172.16.10.0/24"
 
 
@@ -36,12 +39,12 @@ resource "aws_network_interface" "foo" {
 }
 
 resource "aws_instance" "my-terra-ec3" {
-  ami = "ami-0dba2cb6798deb6d8"
+  ami           = "ami-0dba2cb6798deb6d8"
   instance_type = "t2.micro"
-  key_name = "only"
-  
+  key_name      = "only"
+
   network_interface {
     network_interface_id = aws_network_interface.foo.id
-    device_index = 0
+    device_index         = 0
   }
 }
